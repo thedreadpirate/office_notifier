a40233139b79b71b18a12333b043f03acb6f049e
Keith <obrienkeithd@gmail.com>
2014-04-21 21:51:02 -0400

    working mongo logging

--- mongo_logger.rb
+++ mongo_logger.rb
@@ -1,15 +1,18 @@
 require 'mongo'
+require 'json'
 require_relative('./queue_interface')
 
 client = Mongo::Connection.new
-db = client['notifications_db']
-coll = db['office_notifications']
+db = client.db('notifications_db')
+coll = db.collection('office_notifications')
 
 receiver = QueueInterface.new
 
 puts "Listening to topic: #{ARGV}"
 
 receiver.subscribe(ARGV) do |delivery_info, properties, body|
-  puts "inserting: #{body}"
-  coll.insert(body)
+  puts "inserting: #{delivery_info}"
+  coll.update({"_id" => delivery_info[:routing_key]}, {'$push' => { "notifications" => JSON.parse(body)}}, {:upsert => true})
 end
+
+puts "In db: #{coll.find}"
